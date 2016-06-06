ce1b66de1e66473f38f70399f3e5a800bbdd1985
kentaro.maeda <many.front@gmail.com>
2016-06-06 11:28:39 +0900

    migrate to play 2.5.3

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -3,24 +3,14 @@
 
 # --- !Ups
 
-create table T_USER (
-  id                        varchar(255) not null,
-  hash_password             varchar(255),
-  constraint pk_T_USER primary key (id))
-;
-
-create sequence T_USER_seq;
-
-
+create table t_user (
+  id                            varchar(255) not null,
+  hash_password                 varchar(255),
+  constraint pk_t_user primary key (id)
+);
 
 
 # --- !Downs
 
-SET REFERENTIAL_INTEGRITY FALSE;
-
-drop table if exists T_USER;
-
-SET REFERENTIAL_INTEGRITY TRUE;
-
-drop sequence if exists T_USER_seq;
+drop table if exists t_user;
 
